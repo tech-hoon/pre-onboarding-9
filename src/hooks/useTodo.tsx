bb9b15c8de6e2e2cc5edb1ee9ae9fdd15ad3ece2
Taekhun <nth9708@naver.com>
2021-09-02 02:19:28 +0900

    Refactor: useTodo Hook

---
+++ src/hooks/useTodo.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from 'store';
+import {
+  ADD_TODO,
+  EDIT_TODO,
+  FETCH_TODOS,
+  TOGGLE_TODO,
+  REMOVE_TODO,
+  TodoTypes,
+} from 'store/actions/types';
+
+export const useTodo = () => {
+  const { todoList, loading } = useSelector((state: RootState) => state.todos);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    function onFetch() {
+      dispatch({ type: FETCH_TODOS });
+    }
+
+    onFetch();
+  }, []);
+
+  function onAdd(content: string) {
+    dispatch({ type: ADD_TODO, prevTodoList: todoList, content });
+  }
+
+  function onEdit(content: string, todo: TodoTypes, callback: () => void) {
+    dispatch({ type: EDIT_TODO, prevTodoList: todoList, todo: { ...todo, content } });
+    callback();
+  }
+
+  function onToggle(todo: TodoTypes) {
+    dispatch({ type: TOGGLE_TODO, prevTodoList: todoList, todo: todo });
+  }
+
+  function onRemove(todo: TodoTypes) {
+    dispatch({ type: REMOVE_TODO, prevTodoList: todoList, todo });
+  }
+
+  return { todoList, loading, onAdd, onEdit, onToggle, onRemove };
+};
