ae4eec0f66f3278a25737edb18a44e10a5c02166
Taekhun <nth9708@naver.com>
2021-09-01 17:47:45 +0900

    Feat: JSON-Server를 이용하여  mock server와 연결

---
+++ src/api/fetch.ts
@@ -0,0 +1,67 @@
+import { TodoTypes } from 'store/actions/types';
+import { BASE_URL } from 'utils/config';
+
+export async function getFetchTodos(url: string) {
+  try {
+    const res = await fetch(`${BASE_URL}${url}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    return await res.json();
+  } catch (error) {
+    throw new Error();
+  }
+}
+
+export async function postAddTodo(url: string, newTodo: TodoTypes) {
+  try {
+    const res = await fetch(`${BASE_URL}${url}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newTodo),
+    });
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    throw new Error();
+  }
+}
+
+export async function postToggleTodo(url: string, newTodo: TodoTypes) {
+  try {
+    const res = await fetch(`${BASE_URL}${url}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newTodo),
+    });
+
+    return await res.json();
+  } catch (error) {
+    throw new Error();
+  }
+}
+
+export async function postRemoveTodo(url: string) {
+  const id = url.split(`/`)[2];
+
+  try {
+    const res = await fetch(`${BASE_URL}${url}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id }),
+    });
+
+    const result = await res.json();
+    return result;
+  } catch (error) {
+    throw new Error();
+  }
+}
